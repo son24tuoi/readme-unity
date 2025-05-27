2ac168e399cac62eee6d6598fab21f0e225582f0
son24tuoi <son23tuoi@gmail.com>
2025-05-27 23:02:34 +0700

    + Add namespace

--- Assets/Readme/Info/Scripts/Readme.cs
+++ Assets/Readme/Info/Scripts/Readme.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using UnityEngine;
 
-public class Readme : ScriptableObject
+namespace One.Readme
 {
-	public Texture2D icon;
-	public string title;
-	public Section[] sections;
-	public bool loadedLayout;
-	[Serializable]
-	public class Section
+	public class Readme : ScriptableObject
 	{
-		public string heading, text, linkText, url;
+		public Texture2D icon;
+		public string title;
+		public Section[] sections;
+		public bool loadedLayout;
+		[Serializable]
+		public class Section
+		{
+			public string heading, text, linkText, url;
+		}
 	}
 }
